75ef4280382de63cd300d1c96648886fd532e165
rod773 <rodrigoarcadio1@gmail.com>
2024-06-17 12:30:07 +0200

    update

--- header.php
+++ header.php
@@ -12,42 +12,49 @@
 ?>
 <!doctype html>
 <html <?php language_attributes(); ?>>
-<head>
-	<meta charset="<?php bloginfo( 'charset' ); ?>">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<link rel="profile" href="https://gmpg.org/xfn/11">
 
-	<?php wp_head(); ?>
+<head>
+    <meta charset="<?php bloginfo( 'charset' ); ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="profile" href="https://gmpg.org/xfn/11">
+
+    <?php wp_head(); ?>
 </head>
 
-<body <?php body_class('text-gray-700'); ?>>
-<?php wp_body_open(); ?>
-<div id="page" class="site">
-	<a class="skip-link screen-reader-text" href="#primary"><?php esc_html_e( 'Skip to content', 'wptailwindtheme' ); ?></a>
+<body <?php body_class('text-red-700'); ?>>
+    <?php wp_body_open(); ?>
+    <div id="page" class="site">
+        <a class="skip-link screen-reader-text"
+            href="#primary"><?php esc_html_e( 'Skip to content', 'wptailwindtheme' ); ?></a>
 
-	<header id="masthead" class="site-header">
-		<div class="site-branding">
-			<?php
+        <header id="masthead" class="site-header">
+            <div class="site-branding">
+                <?php
 			the_custom_logo();
 			if ( is_front_page() && is_home() ) :
 				?>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-				<?php
+                <h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>"
+                        rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+                <?php
 			else :
 				?>
-				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-				<?php
+                <p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>"
+                        rel="home"><?php bloginfo( 'name' ); ?></a></p>
+                <?php
 			endif;
 			$wptailwindtheme_description = get_bloginfo( 'description', 'display' );
 			if ( $wptailwindtheme_description || is_customize_preview() ) :
 				?>
-				<p class="site-description"><?php echo $wptailwindtheme_description; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></p>
-			<?php endif; ?>
-		</div><!-- .site-branding -->
+                <p class="site-description">
+                    <?php echo $wptailwindtheme_description; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?>
+                </p>
+                <?php endif; ?>
+            </div><!-- .site-branding -->
 
-		<nav id="site-navigation" class="main-navigation">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'wptailwindtheme' ); ?></button>
-			<?php
+            <nav id="site-navigation" class="main-navigation">
+                <button class="menu-toggle" aria-controls="primary-menu"
+                    aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'wptailwindtheme' ); ?></button>
+                <?php
 			wp_nav_menu(
 				array(
 					'theme_location' => 'menu-1',
@@ -55,5 +62,5 @@
 				)
 			);
 			?>
-		</nav><!-- #site-navigation -->
-	</header><!-- #masthead -->
+            </nav><!-- #site-navigation -->
+        </header><!-- #masthead -->
